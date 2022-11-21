940366e9a7c4ce07600c610ebf68ec7d6d8e2788
innovationstechn <36240713+umar12Fa@users.noreply.github.com>
2022-11-21 10:45:09 +0500

    Initial commit

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const appId = '288074fad2154d0fb616e15872434dbb';
+const token = '007eJxTYJjMsihrXjeXmX2oyrNb0WX/5vt0vJ2iwlXQs3vWaemma5EKDEYWFgbmJmmJKUaGpiYpBmlJZoZmqYamFuZGJsYmKUlJmx+VJDcEMjK8227NysgAgSA+J4OhkbGJqZm5hSUDAwDH8B/Z';
